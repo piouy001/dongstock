c3b878b899c0fdd60da3a8c46ff81bb0a42493c2
piouy001 <piouy_@naver.com>
2023-11-03 15:10:44 +0900

    feat : import order 적용, lint룰 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,26 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  plugins: ["react", "react-hooks", 'react-refresh',"import",],
+  env: { browser: true, es2020: true, node: true },
+  plugins: ["react", "react-hooks", "react-refresh", "import"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
     "plugin:react/recommended",
-    "prettier"
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  parser: '@typescript-eslint/parser',
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   overrides: [
     {
       files: ["*.ts", "*.tsx"],
@@ -29,9 +39,50 @@ module.exports = {
     },
   ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react/react-in-jsx-scope": "off",
+    "import/no-named-as-default": "off",
+    "no-unused-vars": "warn",
+    "import/extensions": "off",
+    "import/no-named-as-default": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/no-unresolved": "off",
+    "react-hooks/exhaustive-deps": "warn",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal"],
+        pathGroups: [
+          {
+            pattern: "+(react)",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "components/**",
+            group: "internal",
+            position: "before",
+          },
+          {
+            pattern: "+(hooks|contexts)/**",
+            group: "internal",
+          },
+          {
+            pattern: "+(assets|constants|model|queries|services|styles|theme|routeConfig)/**",
+            group: "internal",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react", "unknown"],
+        alphabetize: {
+          order: "asc",
+        },
+        "newlines-between": "always",
+      },
     ],
   },
-}
+  settings: {
+    react: { version: "detect" },
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
