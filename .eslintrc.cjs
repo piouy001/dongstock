de7903efa81cf5eb56e676b52532413cb1108011
piouy001 <piouy_@naver.com>
2023-11-06 23:45:14 +0900

    bugfix : eslint extends 내용 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,6 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
   ],
